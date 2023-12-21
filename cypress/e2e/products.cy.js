1cd267b00583e7de60e591396f30ea940e5ba083
Thiago Cesar Andrade <thiago.andrade-ext2@ab-inbev.com>
2023-12-21 16:34:09 -0300

    inserindo comando para criar sessão

--- cypress/e2e/products.cy.js
+++ cypress/e2e/products.cy.js
@@ -1,15 +1,13 @@
-const login = require("../fixtures/login.json")
 import produtosPage from "../support/page_objects/produtos.page"
 import your_cartPage from "../support/page_objects/your_cart.page"
 
 describe('Testar feature Produtos', () => {
   
   beforeEach(() => {
-    cy.visit("/")
+    cy.autoLogin()
   })
     
   it('Inserir produto no carrinho', () => {
-    cy.login(login.standard, login.password)
     produtosPage.clickBtnAddToCart()
     produtosPage.bdgShoppingCart.should('contain', '1')
     produtosPage.clickBtnCart()
@@ -17,7 +15,6 @@ describe('Testar feature Produtos', () => {
   })
 
   it('Remover produto do carrinho', () => {
-    cy.login(login.standard, login.password)
     produtosPage.clickBtnAddToCart()
     produtosPage.bdgShoppingCart.should('contain', '1')
     produtosPage.assertBtnRemoveFromCartVisible()
@@ -27,7 +24,6 @@ describe('Testar feature Produtos', () => {
   })
 
   it('Adicionar produto no carrinho pela página do produto', () => {
-    cy.login(login.standard, login.password)
     produtosPage.clickImgProduto()
     produtosPage.clickBtnAddToCart()
     produtosPage.bdgShoppingCart.should('contain', '1')
@@ -36,7 +32,6 @@ describe('Testar feature Produtos', () => {
   })
 
   it('Remover produto do carrinho pela página do produto e voltar a página de produtos', () => {
-    cy.login(login.standard, login.password)    
     produtosPage.clickBtnAddToCart()
     produtosPage.bdgShoppingCart.should('contain', '1')
     produtosPage.clickImgProduto()
@@ -49,7 +44,6 @@ describe('Testar feature Produtos', () => {
   })
 
   it('Validar adicionar e remover todos os produtos no carrinho', () => {
-    cy.login(login.standard, login.password)
     produtosPage.clickBtnAddToCartAllProducts()
     produtosPage.bdgShoppingCart.should('contain', '6')
     produtosPage.clickBtnRemoveFromCartAllProducts()
@@ -58,24 +52,20 @@ describe('Testar feature Produtos', () => {
   })
 
   it('Validar ordenação padrão em ordem alfabética crescente', () => {
-    cy.login(login.standard, login.password)
     produtosPage.validateSortedProductsAtoZ()
   })
   
   it('Ordenar produtos em ordem alfabética decrescente', () => {
-    cy.login(login.standard, login.password)
     produtosPage.orderByNameZtoA()
     produtosPage.validateSortedProductsZtoA()
   })
 
   it('Ordenar produtos em preço do menor para o maior', () => {
-    cy.login(login.standard, login.password)
     produtosPage.orderByPriceLowtoHigh()
     produtosPage.validateSortedProductsLowtoHigh()
   })
 
   it('Ordenar produtos em preço do menor para o maior', () => {
-    cy.login(login.standard, login.password)
     produtosPage.orderByPriceHightoLow()
     produtosPage.validateSortedProductsHightoLow()
   })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,7 +1,13 @@
 import loginpage from "../support/page_objects/login.page"
+const login = require("../fixtures/login.json")
 
 Cypress.Commands.add('login', (usuario, senha) => {
     loginpage.txtUsername.type(usuario)
     loginpage.txtPassword.type(senha, {log:false})
     loginpage.btnLogin.click()    
-});
+})
+
+Cypress.Commands.add('autoLogin', () => {
+    cy.setCookie('session-username', login.standard, login.password)
+    cy.visit('/inventory.html', {failOnStatusCode: false})   
+})
